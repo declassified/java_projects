7c3837862fb0ea53be9d9f52261fefa8cba159b3
mabado <mathewabado@gmail.com>
2015-06-20 15:38:51 -0400

    Add PriorityQueue.java, defining an interface for a priority queue.

---
+++ PriorityQueue/PriorityQueue.java
@@ -0,0 +1,8 @@
+
+public interface PriorityQueue <T extends Comparable<T>> {
+
+	public void push(PriorityQueueEntry<T> newEntry);
+	public PriorityQueueEntry<T> pop();
+	public PriorityQueueEntry<T> peek();
+	
+}
